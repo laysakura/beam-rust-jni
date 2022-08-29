f8cb6fb8de97a869f41526f6ca1985331ae527f3
Sho Nakatani <lay.sakura@gmail.com>
2022-08-29 14:22:14 +0900

    feat: invoke my class from Rust

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,30 @@
-use j4rs::{errors::J4RsError, JvmBuilder};
+use j4rs::{errors::J4RsError, ClasspathEntry, JvmBuilder};
 
 fn main() -> Result<(), J4RsError> {
+    let entry = ClasspathEntry::new(
+        "/Users/sho.nakatani/.ghq/src/github.com/laysakura/beam-rust-jni/lib/build/libs/lib.jar",
+    );
+
     // Create a JVM
-    let jvm = JvmBuilder::new().build()?;
+    let jvm = JvmBuilder::new().classpath_entry(entry).build()?;
 
     // Create a java.lang.String instance
-    let string_instance = jvm.create_instance(
-        "java.lang.String", // The Java class to create an instance for
-        &Vec::new(), // The `InvocationArg`s to use for the constructor call - empty for this example
+    let my_class_instance = jvm.create_instance(
+        "beam.rust.jni.Library",
+        &Vec::new(), // The `InvocationArg`s to use for the invocation - empty for this example
     )?;
 
     // The instances returned from invocations and instantiations can be viewed as pointers to Java Objects.
     // They can be used for further Java calls.
     // For example, the following invokes the `isEmpty` method of the created java.lang.String instance
-    let boolean_instance = jvm.invoke(
-        &string_instance, // The String instance created above
-        "isEmpty",        // The method of the String instance to invoke
-        &Vec::new(),      // The `InvocationArg`s to use for the invocation - empty for this example
-    )?;
+    let boolean_instance = jvm.invoke(&my_class_instance, "someLibraryMethod", &Vec::new())?;
 
     // If we need to transform an `Instance` to Rust value, the `to_rust` should be called
     let rust_boolean: bool = jvm.to_rust(boolean_instance)?;
     println!(
-        "The isEmpty() method of the java.lang.String instance returned {}",
+        "The someLibraryMethod() method of the me.laysakura.MyClass instance returned {}",
         rust_boolean
     );
-    // The above prints:
-    // The isEmpty() method of the java.lang.String instance returned true
 
     // Static invocation
     let _static_invocation_result = jvm.invoke_static(
--- src/main/me/laysakura/MyClass.java
+++
@@ -1,8 +0,0 @@
-package me.laysakura;
-
-class MyClass {
-    public boolean hi() {
-        System.out.println("I'm MyClass::boolean()");
-        return true;
-    }
-}
